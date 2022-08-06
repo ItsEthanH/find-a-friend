4aaaee7bec6f8186c1a846aab3968c37dff75664
Ethan Hawes <ethanhawes@outlook.com>
2022-08-06 20:59:04 +0100

    search all page completed

--- src/pages/search-all/SearchAllBrowseCard.js
+++ src/pages/search-all/SearchAllBrowseCard.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function SearchAllBrowseCard() {
-  return <div>SearchAllBrowseCard</div>;
+import Card from '../../components/ui/Card';
+
+import classes from './styles/SearchAllBrowseCard.module.css';
+
+function SearchAllBrowseCard(props) {
+  return (
+    <Card styles={classes.card}>
+      <div className={classes.image}>
+        <img src={props.image} alt="Dog" />
+      </div>
+      <div className={classes.text}>
+        <h4>{props.name}</h4>
+        <p>{props.available} Available</p>
+      </div>
+    </Card>
+  );
 }
 
 export default SearchAllBrowseCard;
