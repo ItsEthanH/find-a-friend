a04887de788d0950ec48b2dd5a26a3c2039e106c
Ethan Hawes <ethanhawes@outlook.com>
2022-08-07 12:13:33 +0100

    added a single searchbar for potental future use

---
+++ src/components/ui/SingleSearchbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import classes from './styles/SingleSearchbar.module.css';
+
+function SingleSearchbar(props) {
+  return (
+    <form className={classes.form}>
+      <label htmlFor={props.name}>
+        <img src={props.icon} alt="Search" />
+      </label>
+      <input
+        placeholder={props.placeholder}
+        id={props.name}
+        name={props.name}
+        type="text"
+        aria-label={props['aria-label']}
+      />
+    </form>
+  );
+}
+
+export default SingleSearchbar;
