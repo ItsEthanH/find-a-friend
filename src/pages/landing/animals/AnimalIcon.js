d3fba6efba8a6f9b4bc8f18ec18b6544c8519fca
Ethan Hawes <ethanhawes@outlook.com>
2022-08-08 12:20:05 +0100

    small ui improvements

--- src/pages/landing/animals/AnimalIcon.js
+++ src/pages/landing/animals/AnimalIcon.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import AccentButton from '../../../components/ui/AccentButton';
-
 import classes from './styles/AnimalIcon.module.css';
+import buttonClasses from '../../../components/ui/styles/AccentButton.module.css';
 
 function AnimalIcon(props) {
   return (
     <button className={classes.icon}>
       <img src={props.image} alt={props.animal} />
-      <AccentButton>Find a {props.animal}</AccentButton>
+      <p className={buttonClasses.button}>Find a {props.animal}</p>
     </button>
   );
 }
