cce8c3bba7cdd211f613ad42bc1465d0a87a1825
Ethan Hawes <ethanhawes@outlook.com>
2022-08-15 13:38:02 +0100

    api auth logic ported to redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from './store/auth';
 import { Outlet } from 'react-router-dom';
 
 import Header from './components/header/Header';
@@ -7,6 +9,13 @@ import Footer from './components/footer/Footer';
 import './assets/global.css';
 
 function App() {
+  const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authActions.checkTokenInCookie());
+  }, [dispatch, token]);
+
   return (
     <>
       <Header />
