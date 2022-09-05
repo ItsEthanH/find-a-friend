b9b274d8e3260a74c5a3fbbe2d9682f8bb426ae1
Ethan Hawes <ethanhawes@outlook.com>
2022-09-05 12:10:25 +0100

    added fullscreen feature

--- src/pages/pet-view/PetImages.js
+++ src/pages/pet-view/PetImages.js
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper';
 
+import Backdrop from '../../components/cards-and-sections/Backdrop';
+
+import fullscreenIcon from '../../assets/svgs/pet-view/fullscreen.svg';
 import classes from './styles/PetImages.module.css';
 import 'swiper/css';
 
 function PetImages({ photos }) {
   const [image, setImage] = useState(1);
+  const [isFullscreen, setIsFullscreen] = useState(false);
 
   function slideInitialiseHandler(event) {
     setImage(event.activeIndex + 1);
@@ -16,30 +20,49 @@ function PetImages({ photos }) {
     setImage(event.activeIndex + 1);
   }
 
+  function fullscreenClickHandler() {
+    setIsFullscreen((prevState) => !prevState);
+  }
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+    body.style.overflow = isFullscreen ? 'hidden' : 'auto';
+  }, [isFullscreen]);
+
   const renderedPhotos = photos.map((image) => (
     <SwiperSlide>
       <img src={image.full} alt="" />
     </SwiperSlide>
   ));
 
-  return (
-    <div className={classes.slider}>
+  const componentStyles = `${classes.slider} ${isFullscreen ? classes.fullscreen : ''}`;
+  const petImagesComponent = (
+    <div className={componentStyles}>
       <Swiper
-        autoHeight={true}
+        autoHeight={isFullscreen ? false : true}
         modules={[Navigation, Pagination]}
         navigation
         pagination
-        spaceBetween={50}
         slidesPerView={1}
         onInit={slideInitialiseHandler}
         onSlideChange={slideChangeHandler}
       >
         {renderedPhotos}
       </Swiper>
+      <button onClick={fullscreenClickHandler}>
+        <img src={fullscreenIcon} alt="Go fullscreen" />
+      </button>
       <p className={classes.number}>
         {image} of {photos.length}
       </p>
     </div>
+  );
+
+  return (
+    <>
+      {isFullscreen && <Backdrop onClick={fullscreenClickHandler}>{petImagesComponent}</Backdrop>}
+      {!isFullscreen && petImagesComponent}
+    </>
   );
 }
 
