6ead3967e04309e2ec48e77faf66187a584af2d3
Ethan Hawes <ethanhawes@outlook.com>
2022-09-07 11:47:46 +0100

    files and folders

---
+++ src/pages/org-view/_OrgViewPage.js
@@ -0,0 +1,11 @@
+import classes from './styles/OrgViewPage.module.css';
+
+function _OrgViewPage(props) {
+  return (
+    <main>
+      <p>Org View</p>
+    </main>
+  );
+}
+
+export default _OrgViewPage;
